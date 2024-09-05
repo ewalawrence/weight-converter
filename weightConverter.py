6939a85a5cbe42cbc9a09022a09c43c87704f7ad
Lawrence Ewa <ewalawrence1991@gmail.com>
2024-09-05 22:31:30 +0100

    adjusted the units

--- weightConverter.py
+++ weightConverter.py
@@ -5,12 +5,12 @@ unitOfConversion = input("kilograms or Pounds? (K or P): ").upper()
 
 if unitOfConversion == "K":
     weight = round(weight * 2.205, 1)
-    unit = "Kg"
-    print(f"Your current weight is: {weight}{unit}")
+    unit = "Lps"
+    print(f"Your current weight is: {round(weight, 1)}{unit}")
 elif unitOfConversion == "P":
     weight = round(weight / 2.205, 1)
-    unit = "Lps"
-    print(f"Your current weight is: {weight}{unit}")
+    unit = "kg"
+    print(f"Your current weight is: {round(weight, 1)}{unit}")
 
 else:
     print(f"{unitOfConversion} is not valid unit of conversion")
